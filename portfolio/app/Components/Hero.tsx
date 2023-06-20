0acfb2ed6e4e75ab6674bb6feea9557455b7bebd
Saurav Pant <pantsaurav005@gmail.com>
2023-06-20 22:57:18 +0530

    Bug Fixed

--- portfolio/app/Components/Hero.tsx
+++ portfolio/app/Components/Hero.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import backgroundImage from "../Assets/Hero_Background.png";
 
-type Props = {};
-
-const Hero = (props: Props) => {
+const Hero = () => {
   return (
-    <div
-      className="bg-cover bg-center h-screen"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    ></div>
+    <div className="h-[80vh] bg-cover bg-center bg-hero" id="Hero_Section">
+      <div className="flex flex-col items-center justify-center h-full text-center">
+        <h1 className="text-4xl font-bold text-white">Saurav Pant</h1>
+        <h2 className="text-2xl font-semibold text-white">Software Engineer</h2>
+      </div>
+    </div>
   );
 };
 
