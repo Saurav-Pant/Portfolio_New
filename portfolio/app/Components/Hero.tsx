3923019b89b54dcb878ddfb5222fbcac8ff135a5
Saurav Pant <pantsaurav005@gmail.com>
2023-06-21 12:25:32 +0530

    Nav Links Set

--- portfolio/app/Components/Hero.tsx
+++ portfolio/app/Components/Hero.tsx
@@ -1,12 +1,46 @@
 import React from "react";
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="h-[80vh] bg-cover bg-center bg-hero" id="Hero_Section">
+    <div
+      className="h-[80vh] bg-cover bg-center bg-hero relative"
+      id="Hero_Section "
+    >
       <div className="flex flex-col items-center justify-center h-full text-center">
         <h1 className="text-4xl font-bold text-white">Saurav Pant</h1>
         <h2 className="text-2xl font-semibold text-white">Software Engineer</h2>
       </div>
+      <ul className="absolute bottom-4 left-4 flex flex-col gap-4">
+        <li className="transition-transform duration-200 ease-in-out transform hover:scale-125 text-white hover:text-black">
+          <Link
+            href="https://github.com/Saurav-Pant"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={32} />
+          </Link>
+        </li>
+        <li className="transition-transform duration-200 ease-in-out transform hover:scale-125 text-white hover:text-sky-600">
+          <Link
+            href="https://twitter.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter size={32} />
+          </Link>
+        </li>
+        <li className="transition-transform duration-200 ease-in-out transform hover:scale-125 text-white hover:text-blue-600">
+          <Link
+            href="https://www.linkedin.com/in/saurav-pant/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin size={32} />
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
