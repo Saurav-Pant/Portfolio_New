8bd598cbf6eb6450794d3552fa95261856b089a6
Saurav Pant <pantsaurav005@gmail.com>
2023-06-21 23:06:01 +0530

    About Us Page

--- portfolio/app/Components/About.tsx
+++ portfolio/app/Components/About.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Heading2 } from "./Heading_Text";
 
-const About = () => {
-  return <div id="about"></div>;
+type Props = {};
+
+const About = (props: Props) => {
+  return (
+    <div id="about">
+      <Heading2>About</Heading2>
+      {/* About Skills */}
+      <div>
+        {/* Text Related Stuff */}
+        <div>
+          <h3>As a Full Stack Developer, I </h3>
+          <p></p>
+        </div>
+        {/* Skills Related Stuff */}
+        <div></div>
+      </div>
+      {/* Photo Section */}
+      <div></div>
+    </div>
+  );
 };
 
 export default About;
