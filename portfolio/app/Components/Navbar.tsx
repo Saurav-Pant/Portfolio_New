58b223fb22b844d91ba413d996b0d71cc3b97339
Saurav Pant <pantsaurav005@gmail.com>
2023-06-21 12:25:44 +0530

    NavLinks Set

--- portfolio/app/Components/Navbar.tsx
+++ portfolio/app/Components/Navbar.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import React, { useState, useEffect } from "react";
 import { RiMenuLine, RiCloseLine } from "react-icons/ri";
 import Link from "next/link";
@@ -32,12 +32,12 @@ const Navbar = (props: Props) => {
   return (
     <div>
       <nav
-        className={`flex items-center justify-between flex-wrap p-6 bg-gradient-to-t from-fad0c4 to-ffd1ff ${
-          isOpen ? "fixed w-full" : ""
+        className={`flex items-center justify-between flex-wrap p-4 bg-gradient-to-r from-white via-blue-50 to-blue-200 ${
+          isOpen ? "fixed w-full z-50" : ""
         }`}
       >
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <span className="font-semibold text-xl tracking-tight">
+        <div className="flex items-center flex-shrink-0 text-white  mr-6">
+          <span className="font-semibold text-xl tracking-tight pl-6">
             <Link href="/">
               <div className="rounded-full overflow-hidden">
                 <Image src={logo} alt="Logo" width={40} height={40} />
@@ -50,25 +50,31 @@ const Navbar = (props: Props) => {
           <div className="block">
             <button
               id="nav-toggle"
-              className="flex items-center px-3 py-2 rounded cursor-default "
+              className="flex items-center px-3 py-2 rounded cursor-default"
               onClick={toggle}
             >
               {isOpen ? <RiCloseLine size={20} /> : <RiMenuLine size={20} />}
             </button>
           </div>
         ) : (
-          <div className="sm:flex hidden">
+          <div className="sm:flex hidden pr-10">
             <ul className={isOpen ? "hidden" : "flex"}>
-              <li className="inline-block text-gray-500 hover:text-white mr-4">
+              <li
+                className="inline-block text-gray-500 hover:text-white
+              transition-colors duration-500 ease-in-out mr-4"
+              >
                 <Link href="/">Home</Link>
               </li>
-              <li className="inline-block text-gray-500 hover:text-white mr-4">
+              <li
+                className="inline-block text-gray-500 hover:text-white 
+              transition-colors duration-500 ease-in-out mr-4 "
+              >
                 <Link href="/about">About</Link>
               </li>
-              <li className="inline-block text-gray-500 hover:text-white mr-4">
+              <li className="inline-block text-gray-500 hover:text-white transition-colors duration-500 ease-in-out mr-4">
                 <Link href="/projects">Projects</Link>
               </li>
-              <li className="inline-block text-gray-500 hover:text-white mr-4">
+              <li className="inline-block text-gray-500 hover:text-white transition-colors duration-500 ease-in-out mr-4">
                 <Link href="/contact">Blog</Link>
               </li>
             </ul>
@@ -76,13 +82,15 @@ const Navbar = (props: Props) => {
         )}
 
         {isOpen && (
-          <motion.div
-            className="w-full block flex-grow lg:flex lg:items-center lg:w-auto h-screen relative top-10">
-            <ul className="flex flex-col sm:flex-col justify-center items-center font-light" style={{
-              fontSize: "1.5rem"
-            }}>
+          <motion.div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto h-screen relative top-10 z-40">
+            <ul
+              className="flex flex-col sm:flex-col justify-center items-center font-light"
+              style={{
+                fontSize: "1.5rem",
+              }}
+            >
               <motion.li
-                className="block text-gray-500 hover:text-white mr-4 w-full text-center "
+                className="block text-gray-500 hover:text-white mr-4 w-full text-center"
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.9 }}
                 initial={{ opacity: 0, y: -50 }}
