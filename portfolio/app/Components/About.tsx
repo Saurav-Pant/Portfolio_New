e7b4779317548db50ac0ee32bca9a9525b819971
Saurav Pant <pantsaurav005@gmail.com>
2023-06-22 18:00:16 +0530

    Tooltip added

--- portfolio/app/Components/About.tsx
+++ portfolio/app/Components/About.tsx
@@ -2,8 +2,82 @@ import React from "react";
 import { Heading2 } from "./Heading_Text";
 import Image from "next/image";
 import sp from "../Components/sp.jpg";
+import ReactTooltip from "react-tooltip";
+import "react-tooltip/dist/index.css";
+
+import {
+  SiHtml5,
+  SiCss3,
+  SiTailwindcss,
+  SiJavascript,
+  SiGithub,
+  SiReact,
+  SiNextdotjs,
+  SiTypescript,
+  SiNodedotjs,
+  SiExpress,
+  SiMongodb,
+} from "react-icons/si";
 
 type Props = {};
+
+const skills = [
+  {
+    id: 1,
+    name: "HTML",
+    icon: SiHtml5,
+  },
+  {
+    id: 2,
+    name: "CSS",
+    icon: SiCss3,
+  },
+  {
+    id: 3,
+    name: "Tailwind CSS",
+    icon: SiTailwindcss,
+  },
+  {
+    id: 4,
+    name: "JavaScript",
+    icon: SiJavascript,
+  },
+  {
+    id: 5,
+    name: "GitHub",
+    icon: SiGithub,
+  },
+  {
+    id: 6,
+    name: "React",
+    icon: SiReact,
+  },
+  {
+    id: 7,
+    name: "Next.js",
+    icon: SiNextdotjs,
+  },
+  {
+    id: 8,
+    name: "TypeScript",
+    icon: SiTypescript,
+  },
+  {
+    id: 9,
+    name: "Node.js",
+    icon: SiNodedotjs,
+  },
+  {
+    id: 10,
+    name: "Express",
+    icon: SiExpress,
+  },
+  {
+    id: 11,
+    name: "MongoDB",
+    icon: SiMongodb,
+  },
+];
 
 const About = (props: Props) => {
   return (
@@ -21,9 +95,25 @@ const About = (props: Props) => {
           grow as a developer, and I am excited to work on new and challenging
           projects.
         </p>
-      </div>
-      <div>
-        
+        <div className="flex items-center justify-center pt-5 space-x-6">
+          {skills.map((skill) => {
+            const Icon = skill.icon;
+            return (
+              <div
+                key={skill.id}
+                className="relative inline-block"
+                data-tip={skill.name}
+              >
+                <Icon size={30} />
+                <ReactTooltip
+                  place="bottom"
+                  effect="solid"
+                  className="tooltip bg-gray-800 text-white px-2 py-1 rounded"
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
 
       {/* Image Or Animation Section */}
