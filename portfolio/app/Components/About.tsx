66fa5e447c62dd87bf84021f4b40077f4c7115fd
Saurav Pant <pantsaurav005@gmail.com>
2023-06-22 17:07:20 +0530

    Image Added

--- portfolio/app/Components/About.tsx
+++ portfolio/app/Components/About.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Heading2 } from "./Heading_Text";
+import Image from "next/image";
+import sp from "../Components/sp.jpg";
 
 type Props = {};
 
 const About = (props: Props) => {
   return (
-    <div className="flex flex-col md:flex-row min-h-screen">
-      <Heading2>About</Heading2>
-      <div className="flex flex-col justify-center items-center text-center md:w-1/2 sm:pt-20">
-        <h1 className="text-2xl font-semibold">
+    <div className="flex flex-col md:flex-row min-h-[80vh] sm:pl-10 md:pl-20 sm:pt-16">
+      <div className="flex flex-col justify-center items-start text-left md:w-1/2">
+        <Heading2>About</Heading2>
+        <h1 className="text-2xl font-semibold pt-5">
           As a front-end developer, I specialize in building and maintaining the
           user interface of web applications.
         </h1>
-        <p>
+        <p className="pt-5">
           Hi, my name is Ali Reza and I am a web developer with over 2 years of
           experience in the field. I specialize in front-end development and
           have a strong background in ReactJS. I am always looking to learn and
@@ -20,10 +22,15 @@ const About = (props: Props) => {
           projects.
         </p>
       </div>
+      <div>
+        
+      </div>
 
       {/* Image Or Animation Section */}
       <div className="flex justify-center items-center md:w-1/2">
-        <div>{/* Place your image or animation component here */}</div>
+        <div>
+          <Image src={sp} alt="Picture of the author" />
+        </div>
       </div>
     </div>
   );
