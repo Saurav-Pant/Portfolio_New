3fd67fa576bde0bf43138f094d8589eadf973a92
Saurav Pant <pantsaurav005@gmail.com>
2023-06-24 09:50:06 +0530

    through library scroll smooth added

--- portfolio/app/Components/Navbar.tsx
+++ portfolio/app/Components/Navbar.tsx
@@ -5,14 +5,12 @@ import Link from "next/link";
 import Image from "next/image";
 import logo from "../Assets/logo.png";
 import { motion } from "framer-motion";
-import useSmoothScroll from "../Hooks/ScrollSmooth";
 
 interface Props {}
 
 const Navbar: React.FC<Props> = (props: Props) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isMobile, setIsMobile] = useState<boolean>(false);
-  useSmoothScroll();
 
   const toggle = () => {
     setIsOpen(!isOpen);
