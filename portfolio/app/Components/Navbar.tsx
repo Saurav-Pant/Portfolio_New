83f58c53d858a3e09ecce838117b67eb5d8d6aff
Saurav Pant <pantsaurav005@gmail.com>
2023-06-27 19:04:54 +0530

    useRouter added

--- portfolio/app/Components/Navbar.tsx
+++ portfolio/app/Components/Navbar.tsx
@@ -1,19 +1,58 @@
-"use client";
+"use client"
 import React, { useState, useEffect } from "react";
 import { RiMenuLine, RiCloseLine } from "react-icons/ri";
-import { Link as ScrollLink } from "react-scroll";
 import Image from "next/image";
+import logo from "../Assets/logo.png";
 import { motion } from "framer-motion";
-import Logo from "../Assets/logo.png";
+import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
+import { useRouter } from 'next/navigation'
 
-interface Props {}
+interface NavLink {
+  id: number;
+  label: string;
+  to: string;
+}
 
-const Navbar: React.FC<Props> = () => {
+const navLinks: NavLink[] = [
+  {
+    id: 1,
+    label: "About",
+    to: "#about",
+  },
+  {
+    id: 2,
+    label: "Projects",
+    to: "#projects",
+  },
+  {
+    id: 3,
+    label: "Blog",
+    to: "blog",
+  },
+];
+
+const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isMobile, setIsMobile] = useState<boolean>(false);
+  const router = useRouter();
 
   const toggle = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleLogoClick = () => {
+    scroll.scrollToTop({
+      spy: true,
+      smooth: true,
+      duration: 500,
+    });
+    setIsOpen(false);
+    router.push("/"); // Update the URL to the homepage
+  };
+
+  const handleNavLinkClick = (to: string) => {
+    setIsOpen(false); // Close the navbar
+    router.push("/" + to); // Update the URL to the corresponding section
   };
 
   useEffect(() => {
@@ -29,23 +68,30 @@ const Navbar: React.FC<Props> = () => {
     };
   }, []);
 
-  const handleNavLinkClick = () => {
-    setIsOpen(false); // Close the navbar
-  };
-
   return (
     <div>
       <nav className="flex items-center justify-between flex-wrap p-4 bg-gradient-to-r from-white via-blue-50 to-blue-200  fixed w-full z-50 top-0 shadow-md">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <div className="flex items-center flex-shrink-0 text-white  mr-6">
           <span className="font-semibold text-xl tracking-tight pl-6">
-            <ScrollLink to="home" smooth={true} duration={500}>
-              <Image
-                src={Logo}
-                width={100}
-                height={100}
-                alt="logo"
-                className="w-12 rounded-full"
-              />
+            <ScrollLink
+              to=""
+              spy={true}
+              smooth={true}
+              offset={70}
+              duration={500}
+            >
+              <div
+                className="rounded-full overflow-hidden"
+                onClick={handleLogoClick}
+              >
+                <Image
+                  src={logo}
+                  alt="Logo"
+                  width={40}
+                  height={40}
+                  className="cursor-pointer"
+                />
+              </div>
             </ScrollLink>
           </span>
         </div>
@@ -62,37 +108,24 @@ const Navbar: React.FC<Props> = () => {
           </div>
         ) : (
           <div className="sm:flex hidden pr-10">
-            <ul className={isOpen ? "hidden" : "flex gap-4"}>
-              <ScrollLink
-                to="about"
-                smooth={true}
-                duration={500}
-                spy={true}
-                offset={-70}
-                activeClass="active"
-              >
-                <li className="font-medium">About</li>
-              </ScrollLink>
-              <ScrollLink
-                to="projects"
-                smooth={true}
-                duration={500}
-                spy={true}
-                offset={-70}
-                activeClass="active"
-              >
-                <li className="font-medium">Projects</li>
-              </ScrollLink>
-              <ScrollLink
-                to="blog"
-                smooth={true}
-                duration={500}
-                spy={true}
-                offset={-70}
-                activeClass="active"
-              >
-                <li className="font-medium">Blog</li>
-              </ScrollLink>
+            <ul className={isOpen ? "hidden" : "flex"}>
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className="inline-block text-gray-500 hover:text-white transition-colors duration-500 ease-in-out mr-4 cursor-pointer"
+                >
+                  <ScrollLink
+                    to={link.to}
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    onClick={() => handleNavLinkClick(link.to)}
+                  >
+                    {link.label}
+                  </ScrollLink>
+                </li>
+              ))}
             </ul>
           </div>
         )}
@@ -116,57 +149,24 @@ const Navbar: React.FC<Props> = () => {
                 fontSize: "1.5rem",
               }}
             >
-              <ScrollLink
-                to="about"
-                smooth={true}
-                duration={500}
-                spy={true}
-                offset={-70}
-                onClick={handleNavLinkClick}
-                activeClass="active"
-              >
+              {navLinks.map((link) => (
                 <motion.li
-                  className="block text-gray-500 hover:text-sky-400 mb-3 cursor-pointer"
+                  key={link.id}
+                  className="block text-gray-500 hover:text-sky-400 mr-4 mb-3 cursor-pointer"
                   whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.9 }}
                 >
-                  About
+                  <ScrollLink
+                    to={link.to}
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    onClick={() => handleNavLinkClick(link.to)}
+                  >
+                    {link.label}
+                  </ScrollLink>
                 </motion.li>
-              </ScrollLink>
-              <ScrollLink
-                to="projects"
-                smooth={true}
-                duration={500}
-                spy={true}
-                offset={-70}
-                onClick={handleNavLinkClick}
-                activeClass="active"
-              >
-                <motion.li
-                  className="block text-gray-500 hover:text-sky-400 mb-3 cursor-pointer"
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.9 }}
-                >
-                  Projects
-                </motion.li>
-              </ScrollLink>
-              <ScrollLink
-                to="blog"
-                smooth={true}
-                duration={500}
-                spy={true}
-                offset={-70}
-                onClick={handleNavLinkClick}
-                activeClass="active"
-              >
-                <motion.li
-                  className="block text-gray-500 hover:text-sky-400 mb-3 cursor-pointer"
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.9 }}
-                >
-                  Blog
-                </motion.li>
-              </ScrollLink>
+              ))}
             </ul>
           </motion.div>
         )}
