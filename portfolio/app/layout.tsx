7e4b9942b8236b00d23e90f2fad41fc00df587ef
Saurav Pant <pantsaurav005@gmail.com>
2023-06-30 21:20:29 +0530

    Theme Provider

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body className="">
         <Navbar />
         {children}
-        <BackToTop/>
+        <BackToTop />
         <Footer />
       </body>
     </html>
