fa1e8c8503541a09a125cd770a1743424b13e2f1
Saurav-Pant <pantsaurav005@gmail.com>
2024-09-17 21:55:38 +0530

    Chore: Projects Updated

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
       "images.res.cloudinary.com",
       "asset.cloudinary.com",
       "res.cloudinary.com",
+      "pub-96d3131e0e7248bf8295a83f682993ab.r2.dev",
     ],
   },
 };
